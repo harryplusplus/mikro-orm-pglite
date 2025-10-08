63f517eaabc1cb56613877f89de39dd5caa6d739
harryplusplus <harryplusplus@gmail.com>
2025-10-08 17:50:02 +0900

    build: Add keepNames options

--- tsup.config.mjs
+++ tsup.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
+  keepNames: true,
   outExtension: ({ format }) => (format === "esm" ? "js" : format),
 });
